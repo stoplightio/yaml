58d61f8bbc8583bc3c55e49f8490e9eb5e8ba853
Jakub Rożek <jakub@stoplight.io>
2020-05-14 03:51:13 +0200

    fix(deps): bump @stoplight/yaml-ast-parser (#46)

--- src/__tests__/safeStringify.spec.ts
+++ src/__tests__/safeStringify.spec.ts
@@ -24,12 +24,12 @@ Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, ad
     expect(safeStringify({ description }, { lineWidth: 200 })).toEqual(`description: >-
   # API information
 
-  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
-  sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+  dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
 
-  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut
-  enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-  quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit
+  esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 `);
   });
 
@@ -71,5 +71,14 @@ b:
     const obj = { value: '012345678' };
 
     expect(safeStringify(obj, { noRefs: true })).toEqual(`value: '012345678'\n`);
+  });
+
+  test('should correctly escape anchors', () => {
+    const obj = { value: 'test' };
+
+    expect(safeStringify([obj, obj])).toEqual(`- &ref_0
+  value: test
+- *ref_0
+`);
   });
 });
